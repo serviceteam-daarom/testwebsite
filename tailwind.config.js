63fef37b9d5e867061008ff460d28d25b6a153e9
Boyd <133669034+OlgaHaring@users.noreply.github.com>
2025-10-19 19:55:05 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,3 +9,29 @@ export default {
   },
   plugins: [],
 }
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        fadeWord: {
+          '0%': { opacity: '0', transform: 'translateY(15px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        fadeIn: {
+          '0%': { opacity: '0', transform: 'translateY(10px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        fadeWord: 'fadeWord 1.2s ease forwards',
+        fadeIn: 'fadeIn 1.2s ease-in-out forwards',
+      },
+    },
+  },
+  plugins: [],
+};
